187aeeb5116a17e3a0b438d90cd65c3b13fa72d4
Elvismelook <elvismelook@gmail.com>
2023-05-16 15:55:56 -0300

    calculadoraok

--- CalculadoraMatemática/script.js
+++ CalculadoraMatemática/script.js
@@ -1,9 +1,31 @@
 
-
+var res = document.getElementById('result')
 
 function insert(num){
     
     
-     document.getElementById('result').innerHTML = num;
+     document.getElementById('result').innerHTML += num;
     
+}
+function limpar(){
+
+     res.innerHTML = ''
+
+}
+
+function delet(){
+     var res = document.getElementById('result').innerHTML
+
+     document.getElementById('result').innerHTML = res.substring(0, res.length -1)
+}
+
+function calculatudo(){
+     var res = document.getElementById('result').innerHTML
+
+     if(res){
+     document.getElementById('result').innerHTML = eval(res)
+}
+else{
+     alert('Nenhum número á calcular...')
+}
 }
